6d49b2b3cc34fdcd4892698e010137d6f9b98efa
cwy007 <chanweiyan007@gmail.com>
2017-12-16 23:11:22 +0800

    product unit test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.1}
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
-    :multiline => true,       # $ end of line  
+    :multiline => true,       # $ end of line
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
 end
