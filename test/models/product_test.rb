24be5d6a9ecc25bc5759ca7f29c8d3ae32c5eea3
cwy007 <chanweiyan007@gmail.com>
2017-12-17 00:05:25 +0800

    test fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -39,7 +39,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'image url' do
-    ok = %w{ fred.gif fred.jpg fred.pn FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
     ok.each do |name|
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
@@ -48,5 +48,16 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(:title => products(:ruby).title,
+                          :description => 'yyy',
+                          :price => 1,
+                          :image_url => 'fred.gif')
+    assert !product.save
+    assert_equal 'has already been taken' , product.errors[:title].join('; ')
+    # assert_equal I18n.translate('activerecord.errors.messages.taken'),
+    #              product.errors[:title].join('; ')
   end
 end
