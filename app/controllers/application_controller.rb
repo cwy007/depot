05b85abbf3ad618d78fa694dc81447deb301c0fa
cwy007 <chanweiyan007@gmail.com>
2017-12-17 13:38:18 +0800

    generate scaffold cart and implement current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    # 只有 controller 可以调用该方法
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
 end
