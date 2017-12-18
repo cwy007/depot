e929333514d72afac53ba0eba9ea651d3ff71e6a
cwy007 <chanweiyan007@gmail.com>
2017-12-19 06:48:38 +0800

    generate scaffold order and add order_id to line_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217143844) do
+ActiveRecord::Schema.define(version: 20171218224341) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20171217143844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
+    t.integer "order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
