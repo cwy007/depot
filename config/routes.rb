510b6706d3f7545d7360ac40854a05ad0ed776f7
cwy007 <chanweiyan007@gmail.com>
2017-12-20 05:50:47 +0800

    implement atom feed for products#who_bought action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  resources :products
+  resources :products do
+    get 'who_bought', on: :member
+  end
   get 'store/index'
 
   root 'store#index', :as => 'store'
