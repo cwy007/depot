1b0b6bc7624a834537d2c9c8cc4a5df23fac91f1
cwy007 <chanweiyan007@gmail.com>
2017-12-20 06:37:54 +0800

    install will_paginate and implement pagination of orders#index view

---
+++ script/load_orders.rb
@@ -0,0 +1,8 @@
+# transaction 事务，作用：加速处理
+# rails runner script/load_orders.rb
+Order.transaction do
+  (1..100).each do |i|
+    Order.create(:name => "Customer #{i}", :address => "#{i} Main Street",
+      :email => "customer-#{i}@example.com", :pay_type => "Check")
+  end
+end
