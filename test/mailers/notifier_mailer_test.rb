a8e2fb724832ed020a51e055afe71e49cecd9b45
cwy007 <chanweiyan007@gmail.com>
2017-12-21 02:12:55 +0800

    implement user stories integration test

--- test/mailers/notifier_mailer_test.rb
+++ test/mailers/notifier_mailer_test.rb
@@ -7,7 +7,7 @@ class NotifierMailerTest < ActionMailer::TestCase
 
   test "order_received" do
     mail = NotifierMailer.order_received(@order)
-    assert_equal "Progmatic Store Order Confirmation", mail.subject
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal [@order.email], mail.to
     assert_equal ["depot@example.com"], mail.from
     assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
@@ -15,7 +15,7 @@ class NotifierMailerTest < ActionMailer::TestCase
 
   test "order_shipped" do
     mail = NotifierMailer.order_shipped(@order)
-    assert_equal "Progmatic Store Order Shipped", mail.subject
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal [@order.email], mail.to
     assert_equal ["depot@example.com"], mail.from
     assert_match  /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
