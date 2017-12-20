798ac87751ff4c3c09c3aefe26b14534c0b9ea36
cwy007 <chanweiyan007@gmail.com>
2017-12-20 23:06:16 +0800

    update test order shipped and order received

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  # 测试时，mail.from 会取 <> 里面的内容，此处为 "depot@example.com"; default :from 做了剪切
   default from: 'Weiyan Chan <depot@example.com>'
   layout 'mailer'
 end
--- test/mailers/notifier_mailer_test.rb
+++ test/mailers/notifier_mailer_test.rb
@@ -1,20 +1,24 @@
 require 'test_helper'
 
 class NotifierMailerTest < ActionMailer::TestCase
+  setup do
+    @order = orders(:one)
+  end
+
   test "order_received" do
-    mail = NotifierMailer.order_received
-    assert_equal "Order received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = NotifierMailer.order_received(@order)
+    assert_equal "Progmatic Store Order Confirmation", mail.subject
+    assert_equal [@order.email], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "order_shipped" do
-    mail = NotifierMailer.order_shipped
-    assert_equal "Order shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = NotifierMailer.order_shipped(@order)
+    assert_equal "Progmatic Store Order Shipped", mail.subject
+    assert_equal [@order.email], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match  /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
   end
 
 end
