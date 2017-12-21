1a4a04dbc24435403666322e19688956a91dda0c
cwy007 <chanweiyan007@gmail.com>
2017-12-21 18:49:50 +0800

    update users controller, views and test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,10 +17,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { hashed_password: @user.hashed_password, name: @user.name, salt: @user.salt } }
+      post users_url, params: { user: { name: 'xdite', password: 'secret', password_confirmation: 'secret' } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to users_url
   end
 
   test "should show user" do
@@ -34,8 +34,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { hashed_password: @user.hashed_password, name: @user.name, salt: @user.salt } }
-    assert_redirected_to user_url(@user)
+    patch user_url(@user), params: { user: { name: @user.name, password: 'secret', password_confirmation: 'secret' } }
+    assert_redirected_to users_url
   end
 
   test "should destroy user" do
